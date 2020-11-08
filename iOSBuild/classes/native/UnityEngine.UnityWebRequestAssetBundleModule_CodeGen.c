a67d02a6272888a82e22bee6d98e3bdf8d5e68e2
alanlau28 <72107214+alanlau28@users.noreply.github.com>
2020-11-08 03:39:34 -0500

    Add files via upload

---
+++ iOSBuild/classes/native/UnityEngine.UnityWebRequestAssetBundleModule_CodeGen.c
@@ -0,0 +1,87 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 UnityEngine.Networking.UnityWebRequest UnityEngine.Networking.UnityWebRequestAssetBundle::GetAssetBundle(System.String,System.UInt32,System.UInt32)
+extern void UnityWebRequestAssetBundle_GetAssetBundle_m576F5BBED26A92207E0C674F41CE7A1EF650D9C7 ();
+// 0x00000002 UnityEngine.Networking.UnityWebRequest UnityEngine.Networking.UnityWebRequestAssetBundle::GetAssetBundle(System.String,UnityEngine.Hash128,System.UInt32)
+extern void UnityWebRequestAssetBundle_GetAssetBundle_m58A9009DE2A012E5CAC7B720575A3EA9BAABB82C ();
+// 0x00000003 System.Void UnityEngine.Networking.DownloadHandlerAssetBundle::.ctor(System.String,System.UInt32,System.UInt32)
+extern void DownloadHandlerAssetBundle__ctor_m8C55E1839E015D0C781159815946930B34A9285D ();
+// 0x00000004 System.Void UnityEngine.Networking.DownloadHandlerAssetBundle::.ctor(System.String,UnityEngine.Hash128,System.UInt32)
+extern void DownloadHandlerAssetBundle__ctor_m24FA06BA705EAA742F4B48F9CEDF83D4836509A8 ();
+// 0x00000005 System.IntPtr UnityEngine.Networking.DownloadHandlerAssetBundle::CreateCached(UnityEngine.Networking.DownloadHandlerAssetBundle,System.String,System.String,UnityEngine.Hash128,System.UInt32)
+extern void DownloadHandlerAssetBundle_CreateCached_mDE651B99D7669D65060E098C31BA6141A1AD578D ();
+// 0x00000006 System.Void UnityEngine.Networking.DownloadHandlerAssetBundle::InternalCreateAssetBundleCached(System.String,System.String,UnityEngine.Hash128,System.UInt32)
+extern void DownloadHandlerAssetBundle_InternalCreateAssetBundleCached_m1C2B86BCE17D78778F176F55759E1D9AAEF94A9F ();
+// 0x00000007 System.Byte[] UnityEngine.Networking.DownloadHandlerAssetBundle::GetData()
+extern void DownloadHandlerAssetBundle_GetData_mEE15F9F5235CA56D352A42360DBB97F4FC991F79 ();
+// 0x00000008 System.String UnityEngine.Networking.DownloadHandlerAssetBundle::GetText()
+extern void DownloadHandlerAssetBundle_GetText_m0144E0707A55ADBBC66C19DC5D6221D3223F84A6 ();
+// 0x00000009 UnityEngine.AssetBundle UnityEngine.Networking.DownloadHandlerAssetBundle::get_assetBundle()
+extern void DownloadHandlerAssetBundle_get_assetBundle_mC9081BC84B18ED108B8AA06C104985F62AFBD589 ();
+// 0x0000000A UnityEngine.AssetBundle UnityEngine.Networking.DownloadHandlerAssetBundle::GetContent(UnityEngine.Networking.UnityWebRequest)
+extern void DownloadHandlerAssetBundle_GetContent_mA39FD9C402592881680017B080D72337F708A287 ();
+// 0x0000000B System.IntPtr UnityEngine.Networking.DownloadHandlerAssetBundle::CreateCached_Injected(UnityEngine.Networking.DownloadHandlerAssetBundle,System.String,System.String,UnityEngine.Hash128&,System.UInt32)
+extern void DownloadHandlerAssetBundle_CreateCached_Injected_mD9AA1830E3F6FF54F0FC5C8C9FF2CABCB8AE027D ();
+static Il2CppMethodPointer s_methodPointers[11] = 
+{
+	UnityWebRequestAssetBundle_GetAssetBundle_m576F5BBED26A92207E0C674F41CE7A1EF650D9C7,
+	UnityWebRequestAssetBundle_GetAssetBundle_m58A9009DE2A012E5CAC7B720575A3EA9BAABB82C,
+	DownloadHandlerAssetBundle__ctor_m8C55E1839E015D0C781159815946930B34A9285D,
+	DownloadHandlerAssetBundle__ctor_m24FA06BA705EAA742F4B48F9CEDF83D4836509A8,
+	DownloadHandlerAssetBundle_CreateCached_mDE651B99D7669D65060E098C31BA6141A1AD578D,
+	DownloadHandlerAssetBundle_InternalCreateAssetBundleCached_m1C2B86BCE17D78778F176F55759E1D9AAEF94A9F,
+	DownloadHandlerAssetBundle_GetData_mEE15F9F5235CA56D352A42360DBB97F4FC991F79,
+	DownloadHandlerAssetBundle_GetText_m0144E0707A55ADBBC66C19DC5D6221D3223F84A6,
+	DownloadHandlerAssetBundle_get_assetBundle_mC9081BC84B18ED108B8AA06C104985F62AFBD589,
+	DownloadHandlerAssetBundle_GetContent_mA39FD9C402592881680017B080D72337F708A287,
+	DownloadHandlerAssetBundle_CreateCached_Injected_mD9AA1830E3F6FF54F0FC5C8C9FF2CABCB8AE027D,
+};
+static const int32_t s_InvokerIndices[11] = 
+{
+	202,
+	1927,
+	35,
+	1928,
+	1929,
+	1930,
+	14,
+	14,
+	14,
+	0,
+	1931,
+};
+extern const Il2CppCodeGenModule g_UnityEngine_UnityWebRequestAssetBundleModuleCodeGenModule;
+const Il2CppCodeGenModule g_UnityEngine_UnityWebRequestAssetBundleModuleCodeGenModule = 
+{
+	"UnityEngine.UnityWebRequestAssetBundleModule.dll",
+	11,
+	s_methodPointers,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+};
